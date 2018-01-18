df3560c74173b66a576ae17ab1a27d39e49e8734
sharan <sharangowda2425@gmail.com>
2018-01-18 22:07:02 +0530

    Angular Animation capabilities!!!

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Optional } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations:[
+    trigger('goals',[
+      transition('* => *',[
+        query(':enter', style({opacity:0}), {optional:true}),
+        query(':enter', stagger('300ms',[
+          animate('.6s ease-in', keyframes([
+            style({opacity:0,transform:'translateY(-75%)',offset:0}),
+            style({opacity:0.5,transform:'translateY(35%)',offset:0.3}),
+            style({opacity:1,transform:'translateY(0)',offset:1})
+          ]))
+        ]), { optional: true }),
+
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({ opacity: 1, transform: 'translateY(0)', offset: 0 }),
+            style({ opacity: 0.5, transform: 'translateY(35%)', offset: 0.3 }),
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 1 })
+          ]))
+        ]), { optional: true }),
+
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 itemCount: number ; //interpolation 
 btnText: string = 'Add an Item'; //property 
 goalText: string = 'My First Life Goal'; //model 
-goals = []; 
+goals = ['My First Life Goal','Climb Mountain', 'Go ice Skiing']; 
 constructor() { }
 
 // Life cycle hook init when compo loaded
@@ -22,5 +46,9 @@ constructor() { }
     this.goalText ="";
     this.itemCount = this.goals.length;
 
+  }
+  removeItem(i){
+    this.goals.splice(i,1);
+    
   }
 }
